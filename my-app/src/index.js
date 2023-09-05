a2c2468009a74ee03865539174a8ccf98e652ba9
Mary Minikh <maryminikh28@gmail.com>
2023-09-05 12:33:47 +0300

    Курс

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/';
+import 'bootstrap/dist/css/bootstrap.css';
+// import {ReactComponent} from ;
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
